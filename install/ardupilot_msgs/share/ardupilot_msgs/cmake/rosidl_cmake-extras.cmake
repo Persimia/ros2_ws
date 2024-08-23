73180c4b910569a29e61183b73d9816bbfe985cf
Sam Kemp <samkemp1996@gmail.com>
2024-08-23 10:38:49 -0400

    first real commit

---
+++ install/ardupilot_msgs/share/ardupilot_msgs/cmake/rosidl_cmake-extras.cmake
@@ -0,0 +1,4 @@
+# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
+
+set(ardupilot_msgs_IDL_FILES "msg/GlobalPosition.idl;srv/ArmMotors.idl;srv/ModeSwitch.idl")
+set(ardupilot_msgs_INTERFACE_FILES "msg/GlobalPosition.msg;srv/ArmMotors.srv;srv/ArmMotors_Request.msg;srv/ArmMotors_Response.msg;srv/ModeSwitch.srv;srv/ModeSwitch_Request.msg;srv/ModeSwitch_Response.msg")
